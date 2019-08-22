d6b1652380ae7577afcf3a5f47a35c59ad9a2318
saymon85 <saymon_bg@hotmail.com>
2019-08-22 16:04:33 +0200

    change url for api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const url = 'http://data.fixer.io/api/latest?access_key=6105ba1cc5faf4a35ed3eb2f0d451faf';
+const url = 'http://www.apilayer.net/api/live?access_key=6105ba1cc5faf4a35ed3eb2f0d451faf';
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
     const response = await axios.get(`${url}&format=1`);
