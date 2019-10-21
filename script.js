14c3e817aed76054d9f0a256800810fbd91628b1
saymon85 <saymon_bg@hotmail.com>
2019-10-21 18:02:02 +0200

    delete dummy option in select HMTL element, add logic to fill select with countries and currency

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-const axios = require('axios');
-const url = 'http://www.apilayer.net/api/live?access_key=6105ba1cc5faf4a35ed3eb2f0d451faf';
+//const axios = require('axios');
+const urlCurrency = 'http://www.apilayer.net/api/live?access_key=6105ba1cc5faf4a35ed3eb2f0d451faf&format=1';
+const urlCountry = 'https://restcountries.eu/rest/v2/currency/';
 
-const getExchangeRate = async (fromCurrency, toCurrency) => {
-    const response = await axios.get(`${url}&format=1`);
-    console.log(response);
+/* const getExchangeRate = async (fromCurrency, toCurrency) => {
+    const response = await axios.get(urlCurrency);
+    const rates = response.data.quotes;
+    console.log(response.data.quotes);
+}
+
+const getCountries = async (currencyCode) => {
+    const response = await axios.get(`${urlCountry}${currencyCode}`);
+    console.log(response.data[0].name);
 }
 
 getExchangeRate();
+getCountries('HRK'); */
+
+// Get list of countries and fill select element with it
+
+async function getCountries(){
+    const select = Array.from(document.querySelectorAll('.countries'));
+    let countriesList = await fetch('https://restcountries.eu/rest/v2/all');
+    countriesList = await countriesList.json();
+    select.map((element) => {
+        countriesList.map( country => {
+            console.log(country);
+            const option = document.createElement('option');
+            option.textContent = `${country.name} - ${country.currencies[0].code}`;
+            option.value = `${country.currencies[0].code}`;
+            element.appendChild(option);
+        })
+
+        
+    })
+
+}
+
+getCountries();
+
+/* const opcije = Array.from(document.querySelectorAll('#from option'));
+console.log(opcije);
+opcije.map(el => console.log(el.value)); */
