9f2bc36009fc2e26351edf01b75f6ee6f2c805d8
saymon85 <saymon_bg@hotmail.com>
2019-10-23 14:54:49 +0200

    add logic for converting value and displaying it

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// Globals
 
 const urlCurrency = 'http://data.fixer.io/api/latest?access_key=fb9c548f646108b674e3ed2114dd2062&format=1';
-const urlCountry = 'https://restcountries.eu/rest/v2/currency/';
+const urlCountry = 'https://restcountries.eu/rest/v2/all';
+const btn = document.querySelector('.btnCalc button');
 
 // Get list of countries and fill select element with it
 
-async function getCountries(){
+const getCountries = async () => {
     const select = Array.from(document.querySelectorAll('.countries'));
-    let countriesList = await fetch('https://restcountries.eu/rest/v2/all');
+    let countriesList = await fetch(urlCountry);
     countriesList = await countriesList.json();
     select.map((element) => {
         countriesList.map( country => {
@@ -18,17 +20,44 @@ async function getCountries(){
     });
 }
 
-getCountries();
-
 // Get base rate for Euro and calculate exchange rate
 
-async function getExchangeRate() {
-    const from = document.querySelector('#from').value;
-    const to = document.querySelector('#to').value;
+const getExchangeRate = async () => {
+    const fromCurrency = document.querySelector('#from').value;
+    const toCurrency = document.querySelector('#to').value;
     const ratesResponse = await fetch(urlCurrency);
     const ratesData = await ratesResponse.json();
+    const rate = (ratesData.rates[toCurrency] / ratesData.rates[fromCurrency]).toFixed(2);
 
-    return ratesData.rates[to] / ratesData.rates[from];
+    convertCurrency(rate, fromCurrency, toCurrency);
+}
+
+// Converting currency and updating DOM
+
+const convertCurrency = async (rate, fromCurrency, toCurrency) => {
+    const inputValue = document.querySelector('#value').value;
+    const convertedValue = inputValue * rate;
+    
+    updateDOM(convertedValue, inputValue, fromCurrency, toCurrency);
 }
 
 
+
+const updateDOM = (data, inputValue, fromCurrency, toCurrency) => {
+    const fromValue = document.querySelector('.inputValue');
+    const convertedValue = document.querySelector('.convertedValue');
+    fromValue.textContent = `${inputValue} ${fromCurrency} = `;
+    convertedValue.textContent = `${data} ${toCurrency}`;
+
+}
+
+// Switch place of currencies ( to on place of from)
+
+const switchPlace = () => {
+    
+}
+
+// event listeners
+
+btn.addEventListener('click', getExchangeRate);
+window.onload = getCountries();
